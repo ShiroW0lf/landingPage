13e4bd34ccf47c962d7e47cbc821ad8a5d977aae
Aswin Lohani <99501631+ShiroW0lf@users.noreply.github.com>
2024-05-06 16:50:35 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Landing Page">
+    <meta name="author" content="Aswin Lohani - Business Analyst@UTPB - 05/02/2024">
+    <title>MyUTPB</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</head>
+<body>
+    <header id="head" class="secondary">
+        <div class="container">
+            <div class="logo-container">
+                <img src="images/myutpb2.png" alt="UTPB Logo" class="logo">
+              
+            </div>
+        </div>
+    </header>
+    <div class="loginbox">
+    <div class="student-box">
+    <i class="fas fa-user-graduate fa-3x"></i>
+    <h2>Students</h2>
+    <p>All active students please log in to the portal below to access your information</p>
+    
+    <a class="btn btn-warning" href="https://my2.utpb.edu/manage/login?realm=&r=/portal/student" role="button">Student Portal Sign In</a>
+    <p class="login-note">Note: Please try <a href="https://my.utpb.edu/clearcache.htm">clearing your browser</a> cache if you are having trouble logging in.</p>
+</div>
+
+            <div class="alumni-box">
+            <i class="fas fa-user-friends fa-3x"></i>
+    <h2>Alumni</h2>
+    <p>Click below for alumni services:</p>
+    <div class="alumni-buttons">
+        <a class="btn btn-info" href="https://www.utpb.edu/utility-navigation/alumni" role="button">Alumni Portal</a>
+        <div style="margin: 10px; visibility: hidden;"></div>
+
+        <div>
+        <a class="btn btn-secondary" href="https://utpb.myonplanu.com/login" role="button">Pay Your Balance</a>
+        <a class="btn btn-secondary" href="https://www.parchment.com/u/registration/32349/institution" role="button">Request Official Transcript</a>
+        <a class="btn btn-secondary" href="https://pbcs-prd.utshare.utsystem.edu/Shibboleth.sso/Login?entityID=https://shibb.utpb.edu/idp/shibboleth&target=https://pbcs-prd.utshare.utsystem.edu/psc/PBCSPRD/EMPLOYEE/SA/c/NUI_FRAMEWORK.PT_AGSTARTPAGE_NUI.GBL?CONTEXTIDPARAMS=TEMPLATE_ID:PTPPNAVCOL%26scname=UTZ_MANAGE_MY_FINANCES%26PanelCollapsible=Y%26PTPPB_GROUPLET_ID=UTZ_PB_MY_FINANCES_TL_FL%26CRefName=UTZ_PB_MY_FINANCES_TGT_FL%26ptgpid=UTZ_S201907161029598856684783" role="button">View 1098t</a>
+        <a class="btn btn-secondary" href="https://app.joinhandshake.com/" role="button">Handshake</a>
+</div>
+<p class="login-note">Inactive students, please utilize the buttons above to access the resources you need.</p>
+    </div>
+
+    </div>
+
+   <!-- info-->
+   <div class="additional-info-box">
+    <div class="info-section">
+        <div class="alert alert-danger" role="alert">
+            <i class="fas fa-exclamation-circle"></i>
+            <h3 class="alert-heading">FalconID Information & Troubleshooting</h3>
+            <p>You need your FalconID to access resources like student portal, Canvas, and email.</p>
+            <p>Your FalconID format: (last name)_(first initial)_(last 5 digits of student ID)</p>
+            <p>Example: Jane Doe, Student ID #: 9001232344, FalconID = doe_j32344</p>
+            <p>If you need to reset or claim your FalconID password, use the FalconID account management tools.</p>
+            <p>For assistance with login issues:</p>
+            <ul>
+                <li>Look up your FalconID <a href="https://falconid.utpb.edu/">here</a>.</li>
+                <li>Claim your FalconID password <a href="https://falconid.utpb.edu/index.php/claim">here</a>.</li>
+                <li>Reset your password <a href="https://falconid.utpb.edu/index.php/pwreset">here</a>.</li>
+                <li>View the login troubleshooting guide <a href="https://falconid.utpb.edu/index.php/sitepages/peoplesoft_login">here</a>.</li>
+            </ul>
+            <p class="login-note"> If you are faculty/staff please go to <a href = "https://utpb.sharepoint.com/Pages/Home.aspx">Falcon4u</a> and use the important link section to access PeopleSoft.</p>
+   
+        </div>
+    </div>
+    
+</div>
+
+
+
+<script type="text/javascript" src="https://ai.ocelotbot.com/embed/standard/276c4a90-59b6-4863-9ad9-0aaac98356a2"></script>
+    <!-- footer -->
+    <div id="bottom-info">
+    <div class="footer-container">
+        <div class="footer1">
+            <div class="widget">
+                <h3 class="widget-title">Need Help?</h3>
+                <div class="widget-body">
+                    <p>Call the Help Line - 432-552-2010 || 
+                        Submit a Help Ticket - <a href="https://help.utpb.edu">https://help.utpb.edu</a> || 
+                        Email - help@utpb.edu
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="footer2">
+            <p>MyUTPB</p>
+            
+        </div>
+    </div>
+</div>
+
+ <!-- Bootstrap JavaScript files -->
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    
+</body>
+</html>
